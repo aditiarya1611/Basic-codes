c76b7a1a2f4d4d954a33f39698c0384ad370fa0a
Aditi Arya <42584196+aditiarya1611@users.noreply.github.com>
2018-08-23 14:14:19 +0530

    Input output

---
+++ gfgio.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //code
+    int i, T, a, b;
+    cin>>T;
+    for(i=0;i<T;i++)
+    {
+        cin>>a>>b;
+        cout<<a*b;
+    }
+    
+    return 0;
+}
