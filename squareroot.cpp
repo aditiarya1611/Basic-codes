e19ffc9fa903d37025aef580085d4c6599df7372
Aditi Arya <42584196+aditiarya1611@users.noreply.github.com>
2018-09-07 17:30:59 +0530

    Square Root

---
+++ squareroot.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+long long int floorSqrt(long long int);
+long long int floorSqrt(long long int x)
+{
+    double result;
+    result=sqrt(x);
+    return result;
+}
+
+int main()
+{
+    int T,i;
+    cin>>T;
+    for(i=1;i<=T;i++)
+    {
+        int num;
+        cin>>num;
+        cout<<floorSqrt(num)<<endl;
+    }
+    return 0;
+}
