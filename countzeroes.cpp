c94f758d7ba5c06c23b26c7f05d9c52986106999
Aditi Arya <42584196+aditiarya1611@users.noreply.github.com>
2018-09-10 23:19:45 +0530

    Count zeroes in a stored matrix

---
+++ countzeroes.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+#define MAX 1000
+int countZeroes(int A[MAX][MAX], int N);
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int n;
+        cin>>n;
+        int A[MAX][MAX];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                cin>>A[i][j];
+            }
+        }
+        cout<<countZeroes(A,n)<<endl;
+    }
+    return 0;
+}
+
+int countZeroes(int A[MAX][MAX], int N)
+{
+    int count=0;
+    for(int i=0;i<N;i++)
+        {
+            for(int j=0;j<N;j++)
+            {
+                if(A[i][j]==0)
+                count++;
+            }
+        }
+        return count;
+}
