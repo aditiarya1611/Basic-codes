ffb3fda004a3f6f4244472ec7c6d60ff00612d73
Aditi Arya <42584196+aditiarya1611@users.noreply.github.com>
2018-09-23 12:32:40 +0530

    Nth Term of Series 1 3 6 10 15 21

---
+++ seriesterm.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int T;
+cin>>T;
+while(T--)
+    {
+        int n;
+        cin>>n;
+        cout<<n*(n+1)/2<<endl;
+    }
+    return 0;
+}
