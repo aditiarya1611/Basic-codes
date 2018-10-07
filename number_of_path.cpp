e28c41fa819d354cb18fba1285a9d8f4838fae2d
Aditi Arya <42584196+aditiarya1611@users.noreply.github.com>
2018-10-07 11:04:09 +0530

    Number of Path

---
+++ number_of_path.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int path(int x, int y)
+{
+    if(x==1 || y==1)
+    return 1;
+    else
+    return path(x-1,y) + path(x,y-1);
+}
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int a,b;
+        cin>>a>>b; 
+        cout<<path(a,b)<<endl;
+    }
+    return 0;
+}
