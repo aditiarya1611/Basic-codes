d5d4766e73990e8f1af85d0294fa18b5e0c3dc35
Aditi Arya <42584196+aditiarya1611@users.noreply.github.com>
2018-10-07 11:03:14 +0530

    Finding Happy Number

---
+++ happy_number.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+bool ishappynumber(int n)
+{
+    if(n==1)
+        return true;
+    else if(n>9)
+    {
+        int sum=0;
+        while(n!=0)
+        {
+            int x=n%10;
+            sum+=(x*x);
+            n=n/10;
+        }
+        if(ishappynumber(sum))
+            return true;
+        else
+            return false;
+    }
+    return false;
+}
+
+void nextsmallest(int n)
+{
+    int i=n+1;
+    while(true)
+    {
+        if(ishappynumber(i)==true)
+        {
+            cout<<i;
+            break;
+        }
+        i++;
+    }
+}
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    nextsmallest(n);
+	    cout<<endl;
+	}
+	return 0;
+}
